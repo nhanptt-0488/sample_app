dcce336962384e2d052561c0ecce1eeff5aa6830
nhanptt-0488 <phan.thi.thanh.nhan@sun-asterisk.com>
2024-07-12 10:50:38 +0700

    chapter 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,11 @@ class UsersController < ApplicationController
     @user = User.new user_params
 
     if @user.save
+      log_in @user
       flash[:success] = t("user_created_successfully")
       redirect_to @user
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
