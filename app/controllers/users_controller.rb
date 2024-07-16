9dc1a1fac0f3ffb4919ca34f5eec8d6797ae08fd
nhanptt-0488 <phan.thi.thanh.nhan@sun-asterisk.com>
2024-07-16 22:42:04 +0700

    chapter 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-  before_action :find_user, only: :show
+  before_action :logged_in_user, excpet: %i(show new create)
+  before_action :set_user, except: %i(index new create)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
 
   def show
     @user
@@ -21,6 +24,50 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @pagy, @users = pagy User.all, items: Settings.users_per_page
+  end
+
+  def edit; end
+
+  def update
+    if @user.update user_params
+      flash[:success] = t "users.update.success"
+      redirect_to @user
+    else
+      flash.now[:danger] = t "users.update.failed"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t "users.destroy.success"
+    else
+      flash[:danger] = t "users.destroy.failed"
+    end
+    redirect_to users_path
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t "users.error.please_log_in"
+    redirect_to login_path, status: :see_other
+  end
+
+  def correct_user
+    return if current_user? @user
+
+    flash[:danger] = t "users.error.not_correct_user"
+    redirect_to root_path, status: :see_other
+  end
+
+  def admin_user
+    redirect_to root_path unless current_user.admin?
+  end
+  
   private
   def user_params
     params.require(:user).permit(User::ATTRIBUTES)
