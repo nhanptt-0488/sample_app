6fbfafbf531415c2e8b885c7b84c0c9ec0fcf739
nhanptt-0488 <phan.thi.thanh.nhan@sun-asterisk.com>
2024-07-16 22:42:04 +0700

    chapter 10

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_15_142827) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_16_153917) do
   create_table "products", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_15_142827) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
   end
 
 end
